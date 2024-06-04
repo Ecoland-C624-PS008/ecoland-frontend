aa7fae5811255824f4adfbfffa2b1c1aeeb43fc7
Nurazmi Dhimas Azizah <96005016+azmiazizah18@users.noreply.github.com>
2024-06-04 07:57:58 +0700

    Update App.jsx
    memambah route login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Body from './components/Body';
 import Cards from './components/Cards';
 import Footer from './components/Footer';
-
+import Login from './components/Login';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Hero />
-      <Body />
-      <Cards />
-      <Footer />
-    </div>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero />
+            <Body />
+            <Cards />
+            <Footer />
+          </>
+        } />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
