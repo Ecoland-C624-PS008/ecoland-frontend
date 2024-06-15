be673b2b6b0bc23f5883b6024278438c188e7172
astrocoding <reepalstudio01@gmail.com>
2024-06-16 01:23:55 +0700

    update homepage: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,15 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-200'>
-    <h1 className='w-full text-3xl font-bold text-green-500'>ECOLAND FOOTER</h1>
-    </div>
-  )
-}
+    const currentYear = new Date().getFullYear();
+
+    return (
+      <div className='max-w-[100%] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-200 bg-[#212529]'>
+        <h1 className='w-full text-3xl font-bold text-white'>ECOLAND FOOTER</h1>
+        <p className='text-sm text-gray-400'>Platform web penyewaan bisnis lahan hijau berbasis di Indonesia.</p>
+        <p className='text-sm text-gray-400'>&copy; {currentYear} Ecoland. All rights reserved.</p>
+      </div>
+    );
+  };
 
 export default Footer
