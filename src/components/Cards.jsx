7fe43e1eea87bcb271c408e6659d9132777bcf4b
astrocoding <reepalstudio01@gmail.com>
2024-06-16 01:38:12 +0700

    update homepage components & add transition button

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,13 +12,13 @@ const Cards = () => {
                     <FaSeedling size="80" className='text-5xl text-green-500 mx-auto pt-5' />
                     <h2 className='text-2xl font-bold text-center py-8'>Mulai Bisnis Hijau</h2>
                     <p className='text-center text-4xl'>mulai sewa</p>
-                    <button className='bg-green-500 text-white font-bold w-[200px] rounded-md font-medium my-6 mx-auto py-3 hover:bg-green-600'>Sewa Lahan</button>
+                    <button className='bg-green-500 text-white font-bold w-[200px] rounded-md font-medium my-6 mx-auto py-3 transition duration-300 ease-in-out hover:bg-green-600'>Sewa Lahan</button>
                 </div>
                 <div className='w-full shadow-md border flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
                     <FaBullhorn size="80" className='text-5xl text-green-500 mx-auto pt-5' />
                     <h2 className='text-2xl font-bold text-center py-8'>Pasang Iklan Lahan</h2>
                     <p className='text-center text-4xl'>mulai iklan</p>
-                    <button className='bg-green-500 text-white font-bold w-[200px] rounded-md font-medium my-6 mx-auto py-3 hover:bg-green-600'>Sewakan Lahan</button>
+                    <button className='bg-green-500 text-white font-bold w-[200px] rounded-md font-medium my-6 mx-auto py-3 transition duration-300 ease-in-out hover:bg-green-600'>Sewakan Lahan</button>
                 </div>
 
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
       <div className='max-w-[100%] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-200 bg-[#212529]'>
-        <h1 className='w-full text-3xl font-bold text-white'>ECOLAND FOOTER</h1>
+        <h1 className='w-full text-3xl font-bold text-white'>EcoLand Indonesia</h1>
         <p className='text-sm text-gray-400'>Platform web penyewaan bisnis lahan hijau berbasis di Indonesia.</p>
-        <p className='text-sm text-gray-400'>&copy; {currentYear} Ecoland. All rights reserved.</p>
+        <p className='text-sm text-gray-400'>&copy; {currentYear} <Link to="/" className='font-bold'>Ecoland</Link>. All rights reserved.</p>
       </div>
     );
   };
