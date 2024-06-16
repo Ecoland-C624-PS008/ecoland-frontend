00376278437ba48b63ddbafe033bcd40e7586308
astrocoding <reepalstudio01@gmail.com>
2024-06-16 20:22:53 +0700

    update /pages jsx

---
+++ src/pages/RentalAdmin.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import Sidebar from '../components/dashboard/Sidebar';
+import Navbar from '../components/dashboard/Navbar';
+
+const RentalAdmin = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex">
+      <Sidebar isOpen={isOpen} />
+      <div className="flex-grow bg-gray-100">
+        <Navbar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+        <div className="p-6 mt-16">
+          <h1 className="text-2xl font-bold mb-4">Histori Sewa Lahan</h1>
+          <p>Test text</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RentalAdmin;
